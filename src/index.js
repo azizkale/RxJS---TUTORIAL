82cb0d2da3cadc4a64146a0876eaaa9b2ea3c3d1
azizkale <azizkale@hotmail.com>
2021-06-26 19:35:59 +0200

    ServerAndRoutersWorkWell

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import opof from "./Routes/Of";
 
 const PORT = process.env.PORT || 4003;
 
 const app = express();
 app.use(cors());
+
+app.use("/", opof);
 
 app.listen(PORT, () => console.log(`Server runs on ${PORT}`));
