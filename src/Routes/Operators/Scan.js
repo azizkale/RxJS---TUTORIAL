d9a14a36a54ea1442dcc3215f89fdf385c1a4040
azizkale <azizkale@hotmail.com>
2021-06-27 12:51:34 +0200

    ScanAdded

---
+++ src/Routes/Operators/Scan.js
@@ -0,0 +1,37 @@
+import express from "express";
+import axios from "axios";
+import { from, Subject } from "rxjs";
+import { scan, map } from "rxjs/operators";
+
+const opScan = express.Router();
+
+opScan.route("/opscan").get(async (req, res, next) => {
+  const array = from([10, 20, 30, 40, 50, 60]);
+
+  // example-1
+  array
+    .pipe(
+      map((val) => {
+        return val;
+      }),
+      scan((total, n) => {
+        return total + n;
+      }),
+      map((sum, index) => sum / index)
+    )
+    .subscribe(console.log);
+
+  // example-2
+  array
+    .pipe(
+      map((val) => {
+        return val.toString();
+      }),
+      scan((acc, curr) => {
+        return acc + " - " + curr;
+      })
+    )
+    .subscribe(console.log);
+});
+
+module.exports = opScan;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import opof from "./Routes/Operators/Of";
 import opfrom from "./Routes/Operators/From";
 import opmap from "./Routes/Operators/Map";
+import opscan from "./Routes/Operators/Scan";
 
 const PORT = process.env.PORT || 4003;
 
@@ -12,5 +13,6 @@ app.use(cors());
 app.use("/", opof);
 app.use("/", opfrom);
 app.use("/", opmap);
+app.use("/", opscan);
 
 app.listen(PORT, () => console.log(`Server runs on ${PORT}`));
